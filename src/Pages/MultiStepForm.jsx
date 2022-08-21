8064bf878a92f129d3bd0ab8f7319d6da6c0126a
adasarpan404 <64941823+adasarpan404@users.noreply.github.com>
2022-08-21 21:25:52 +0530

    Add files via upload

---
+++ src/Pages/MultiStepForm.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import EmailForm from "../Components/MultiStep/EmailForm";
+import NameForm from "../Components/MultiStep/NameForm";
+import PhoneNumber from "../Components/MultiStep/PhoneNumber";
+
+
+
+
+function MultiStepForm() {
+
+  const [emailForm , setEmailForm ] = useState(true)
+  const [nameForm, setNameForm] = useState(false)
+  const [phoneNumberForm, setPhoneNumberForm] = useState(false)
+
+  const handleNameForm=() => {
+    /**
+     * @desc this function is being used to open name form and closing down the email form
+     */
+    setEmailForm(false);
+    setNameForm(true);
+  }
+  const handlePhoneNumberForm = () =>{
+    /**
+     * @desc this function is being used to used open phone number form and closing down the name form
+     */
+    setPhoneNumberForm(true);
+    setNameForm(false);
+  }
+  
+  return (
+    <div>
+      
+      {emailForm?<EmailForm handleNameForm={handleNameForm}/>: null}
+      {nameForm? <NameForm  handlePhoneNumberForm={handlePhoneNumberForm}/>: null}
+      {phoneNumberForm? <PhoneNumber/>: null}
+    
+    </div>
+  );
+}
+
+export default MultiStepForm;
---
+++ src/Pages/PhoneOTPVerification.jsx
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from 'react'
+import OTPInput from "otp-input-react";
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+function PhoneOTPVerification() {
+  const [OTP, setOTP] = useState("");
+  const [isModalVisible, setIsModalVisible] = useState(false)
+  useEffect(()=>{
+    if(OTP === "1234"){
+      setIsModalVisible(true);
+    }
+  },[OTP])
+
+  const handleOk = ()=>{
+    setIsModalVisible(false);
+  }
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  }
+  return (
+
+    <>
+    <OTPInput value={OTP} onChange={setOTP} autoFocus OTPLength={4} otpType="number" disabled={false} />
+    <Modal
+        open={isModalVisible}
+        onClose={handleCancel}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            OTP successfully Verified
+          </Typography>
+          
+        </Box>
+      </Modal>
+    </>
+  )
+}
+
+export default PhoneOTPVerification
